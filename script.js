8b862e6f0a5bcd498c7e31f1a1278cda9b7492d1
bray-hiramis <johnbrianhiramis@gmail.com>
2025-09-15 16:07:08 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// variables
+const celsiusInput = document.getElementById('celsius');
+const fahrenheitInput = document.getElementById('fahrenheit');
+const btnConvert = document.getElementById('btn-convert');
+
+// Formula
+function convertCtoF(celsius) {
+   const formula = celsius * (9/5) + 32;
+   return formula;
+};
+
+// event listener
+btnConvert.addEventListener('click', function() {
+   const celsiusTextValue = celsiusInput.value;
+   fahrenheitInput.value = convertCtoF(celsiusTextValue);
+   console.log(convertCtoF(celsiusTextValue));
+});
+
+celsiusInput.addEventListener('input', function() {
+   if (celsiusInput.value === '') {
+      fahrenheitInput.value = '';
+   }
+})
